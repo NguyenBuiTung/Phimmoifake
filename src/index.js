766c8eda0f4d9787ade4bf0c4d4453b81ab12499
nguyenbuitung <nguyenbuitung4@gmail.com>
2023-03-29 19:49:12 +0700

    addreactlazy

--- src/index.js
+++ src/index.js
@@ -5,55 +5,61 @@ import "./index.css";
 import { Provider } from "react-redux";
 import {
   unstable_HistoryRouter as HistoryRouter,
-  Navigate,
   Route,
   Routes,
 } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import { store } from "./redux/configStore";
 import HomeTemPlate from "./templates/HomeTemPlate";
-import Home from "./Pages/Home";
-import Detail from "./Pages/Detail";
-import DanhSachPhim from "./Components/DanhSachPhim";
-import DanhSachPhimMoi from "./Components/DanhSachPhimMoi";
-import DanhSachPhimVienTuong from "./Components/DanhSachPhimVienTuong";
-import DanhSachPhimKinhDi from "./Components/DanhSachPhimKinhDi";
-import DanhSachPhimHanhDong from "./Components/DanhSachPhimHanhDong";
+// import Home from "./Pages/Home";
+// import Detail from "./Pages/Detail";
+// import DanhSachPhim from "./Components/DanhSachPhim";
+// import DanhSachPhimMoi from "./Components/DanhSachPhimMoi";
+// import DanhSachPhimVienTuong from "./Components/DanhSachPhimVienTuong";
+// import DanhSachPhimKinhDi from "./Components/DanhSachPhimKinhDi";
+// import DanhSachPhimHanhDong from "./Components/DanhSachPhimHanhDong";
+const Home = React.lazy(() => import("./Pages/Home"));
+const Detail = React.lazy(() => import("./Pages/Detail"));
+const DanhSachPhim = React.lazy(() => import("./Components/DanhSachPhim"));
+const DanhSachPhimMoi = React.lazy(() =>
+  import("./Components/DanhSachPhimMoi")
+);
+const DanhSachPhimVienTuong = React.lazy(() =>
+  import("./Components/DanhSachPhimVienTuong")
+);
+const DanhSachPhimKinhDi = React.lazy(() =>
+  import("./Components/DanhSachPhimKinhDi")
+);
+const DanhSachPhimHanhDong = React.lazy(() =>
+  import("./Components/DanhSachPhimHanhDong")
+);
 export const history = createBrowserHistory();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <HistoryRouter history={history}>
-      <Routes>
-        <Route path="/" element={<HomeTemPlate />}>
-          <Route path="/" index element={<Home />}></Route>
-          <Route path="/home" element={<Home />}></Route>
-          <Route path="/phimhot" element={<DanhSachPhim />}></Route>
-          <Route path="/phimmoi" element={<DanhSachPhimMoi />}></Route>
-          <Route
-            path="/phimvientuong"
-            element={<DanhSachPhimVienTuong />}
-          ></Route>
-          <Route path="/phimkinhdi" element={<DanhSachPhimKinhDi />}></Route>
-          <Route path="/phimhanhdong" element={<DanhSachPhimHanhDong />}></Route>
-          <Route path="/detail">
-            <Route path=":id" element={<Detail />}></Route>
+      <React.Suspense>
+        <Routes>
+          <Route path="/" element={<HomeTemPlate />}>
+            <Route path="/" index element={<Home />}></Route>
+            <Route path="/home" element={<Home />}></Route>
+            <Route path="/phimhot" element={<DanhSachPhim />}></Route>
+            <Route path="/phimmoi" element={<DanhSachPhimMoi />}></Route>
+            <Route
+              path="/phimvientuong"
+              element={<DanhSachPhimVienTuong />}
+            ></Route>
+            <Route path="/phimkinhdi" element={<DanhSachPhimKinhDi />}></Route>
+            <Route
+              path="/phimhanhdong"
+              element={<DanhSachPhimHanhDong />}
+            ></Route>
+            <Route path="/detail">
+              <Route path=":id" element={<Detail />}></Route>
+            </Route>
           </Route>
-          {/* <Route path="/" element={<PrivateRoute />}>
-            <Route path="/shop" element={<Profile />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/order" element={<Oder />} />
-          </Route> */}
-          {/* <Route path="/shop" element={<Profile />}></Route>
-          <Route path="/cart" element={<Cart />}></Route>
-          <Route path="/order" element={<Oder />}></Route>
-          <Route path="*" element={<Navigate to={"home"} />}></Route> */}
-        </Route>
-        {/* <Route path="/" element={<TemplateBeautiful />}>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-        </Route> */}
-      </Routes>
+        </Routes>
+      </React.Suspense>
     </HistoryRouter>
     {/* </PersistGate> */}
   </Provider>
